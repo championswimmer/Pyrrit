4d2a1d6e9bb45e5d513f2ba3a134789560021c99
Arnav Gupta <arnav.gupta@cube26.com>
2014-07-28 04:22:24 +0530

    shuffle around stuff

--- pyrrit/__main__.py
+++ pyrrit/__main__.py
@@ -4,24 +4,30 @@ import utils
 
 __author__ = 'arnav'
 
-
-# try :
-if sys.argv[1] == "pull":
-    print("pull")
-    ##TODO: write pull funciton
-
-if sys.argv[1] == "list":
-    import listchanges
-    print("Listing changes from " + listchanges.url)
-    listchanges.show_all_list()
-
-if sys.argv[1] == "upload":
+def print_help():
+    print("Usage : pyrrit [mode [parameters]]")
+    print("Where mode can be \n")
+    print("list [project]          - Show all recent patches on this project")
     print("upload")
-    ##TODO: write upload funciton
-# except:
-#     print("Usage : pyrrit [mode [parameters]]")
-#     print("Where mode can be \n")
-#     print("list [project]          - Show all recent patches on this project")
-#     print("upload")
-#     print("pull [ps#]              - Pull the given patchset(s)")
-#     print("pstest [ps#] [device]   - Pull the given patchset(s), and make a build for device to test")
+    print("pull [ps#]              - Pull the given patchset(s)")
+    print("pstest [ps#] [device]   - Pull the given patchset(s), and make a build for device to test")
+
+if len(sys.argv) < 2 :
+    print_help()
+else:
+    if sys.argv[1] == "help":
+        print_help()
+    if sys.argv[1] == "pull":
+        print("pull")
+        ##TODO: write pull funciton
+
+    if sys.argv[1] == "list":
+        import listchanges
+        print("Listing changes from " + listchanges.url)
+        listchanges.show_all_list()
+
+    if sys.argv[1] == "upload":
+        print("upload")
+        ##TODO: write upload funciton
+
+
