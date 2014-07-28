f9d8f63f77e4498d46afe90b46f17d52cd208360
sauravtom <stomatrix@gmail.com>
2014-07-28 08:40:25 +0530

    Update: cleaned up code in listchanges and handled a corner case in _main_.py

--- pyrrit/__main__.py
+++ pyrrit/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import utils
+import listchanges
 
 __author__ = 'arnav'
 
@@ -14,23 +15,26 @@ def print_help():
     print("pstest [ps#] [device]   - Pull the given patchset(s), and make a build for device to test")
 
 
-if len(sys.argv) < 2:
-    print_help()
-else:
-    if sys.argv[1] == "help":
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
         print_help()
-    if sys.argv[1] == "pull":
+    
+    elif sys.argv[1] == "help":
+        print_help()
+    
+    elif sys.argv[1] == "pull":
         print("pull")
         ##TODO: write pull funciton
 
-    if sys.argv[1] == "list":
-        import listchanges
-
+    elif sys.argv[1] == "list":
         print("Listing changes from " + listchanges.url)
         listchanges.show_all_list()
 
-    if sys.argv[1] == "upload":
+    elif sys.argv[1] == "upload":
         print("upload")
         ##TODO: write upload funciton
 
+    else:
+        print("Bad argument passed")
+        print_help()
 
